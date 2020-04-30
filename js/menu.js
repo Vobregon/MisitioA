4453a5b2896ba1d82727ceade3136796985a1103
Victor Obregon <masterly@hotmail.com>
2020-04-30 00:43:18 -0500

    se termino el responsive

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,6 @@
-var btnMenu = document.getElementById('btnmenu');
-var menu = document.getElementById('menu');
-btnMenu.addEventListener('click', function () {
-	'use strict';
-	menu.classlist.toggle('mostrar');
+let btnMenu = document.getElementById('btnmenu');//creacion de la primera variable
+let menu = document.getElementById('menu');//creacion de la segunda variable
+btnMenu.addEventListener('click', function(){//cuando le des click
+    'use strict';
+    menu.classList.toggle('mostrar');
 });
